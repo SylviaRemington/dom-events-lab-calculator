043d3493464f8d45f5a0098fd85f688cf418586a
Sylvia Remington <sylviaremington@gmail.com>
2025-03-04 22:57:43 -0700

    update

--- app.js
+++ app.js
@@ -34,11 +34,6 @@ separate function for each + - * / ---------*/
 // .row 
 // .button
 
-// I think maybe I don't fully understand the html for this calculator lesson and why it was written that way. Div class confuses me. I get that divs are dividers of content. And that class is a grouping of things. I just don't get why they would make individual numbers in a class together when they are each with individual values.... I wonder why they aren't Ids instead so that each individual number has a different value so that it's easier to calculate. I was planning on creating a math equation through the Dom to manipulate the numbers once they were clicked on. But the way the html was written, it makes it more confusing the way I want to manipulate the dom.
-//I've been doing this coding step by step with the lesson and keep getting error messages.
-
-
-
 
 const calculatorElement = document.querySelector('#calculator');
 console.dir(calculatorElement);
@@ -49,12 +44,12 @@ console.dir(rowElement);
 const displayElement = document.querySelector('.display');
 console.dir(displayElement);
 
-const buttonNumberElement = document.querySelector('.button number')
-console.dir(buttonNumberElement);
+// const buttonNumberElement = document.querySelector('.button number')
+// console.dir(buttonNumberElement); //console null msg
 
-const buttonOperatorElement = document.querySelector('.button operator')
-console.dir(buttonNumberElement);
+// const buttonOperatorElement = document.querySelector('.button operator')
+// console.dir(buttonNumberElement); ///console null msg
 
-const buttonEqualsElement = document.querySelector('.button equals')
-console.dir(buttonNumberElement);
+// const buttonEqualsElement = document.querySelector('.button equals')
+// console.dir(buttonNumberElement); /console null msg
 
