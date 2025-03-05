97821d0164809db21672ffc43844ef814b696bde
Sylvia Remington <sylviaremington@gmail.com>
2025-03-04 19:59:57 -0700

    selecting querySelectors for calculator row display buttonnumber

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 /*-------------------------------- Constants --------------------------------*/
-/* const */
 
 /*-------------------------------- Variables --------------------------------*/
 
@@ -9,10 +8,7 @@
 
 /*-------------------------------- Functions --------------------------------*/
 
-
-//those demarcations above really confuse me... so I'm making my own below
-
-
+//those demarcations above really confuse me... so I'm making my own below...
 /*-----Grab/Select the correct element W/querySelector & make sure you selected the correct element via CONSOLE LOG-----*/
 /*-----Modify text, style, create new elements-----*/
 /*-----ADD EVENT LISTENER & TEST IT OUT VIA CONSOLE.DIR-----*/
@@ -24,5 +20,26 @@ and then I want to have that input show up on the screen.
 So I think I need to create separate functions for each number and 
 separate function for each + - * / ---------*/
 
+/* <id="calculator"> */
+// class="row">
+// class "display">
+// class="button number"
+// class="button operator"
+// class="button equals"
 
 
+const calculatorElement = document.querySelector('#calculator');
+console.dir(calculatorElement);
+
+const rowElement = document.querySelector('.row');
+console.dir(rowElement);
+
+const displayElement = document.querySelector('.display');
+console.dir(displayElement);
+
+
+const buttonnumberElement = document.querySelector('.button-number') //null for this 
+console.dir(buttonnumberElement);//null for this so that means did wrong element
+
+
+class="button number"
