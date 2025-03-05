fb085339939d2a880440201cf9d8a502f0cfb0cf
Sylvia Remington <sylviaremington@gmail.com>
2025-03-04 18:28:03 -0700

    notes to understand html & Jss

--- app.js
+++ app.js
@@ -16,5 +16,13 @@
 /*-----Grab/Select the correct element W/querySelector & make sure you selected the correct element via CONSOLE LOG-----*/
 /*-----Modify text, style, create new elements-----*/
 /*-----ADD EVENT LISTENER & TEST IT OUT VIA CONSOLE.DIR-----*/
-/* ADD ADDTL ITEMS AS NECESSARY */
+/*-----ADD ADDTL Variables and Functions AS NECESSARY----- */
+/*----- What I want to do is create the clicking of each number to = each number, 
+create some math equations with what happens when 
+you add, subtract, multiply, and divide info, 
+and then I want to have that input show up on the screen.
+So I think I need to create separate functions for each number and 
+separate function for each + - * / ---------*/
+
+
 
